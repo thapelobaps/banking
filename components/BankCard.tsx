731a742f8681dfc5d2e8f98025205d89980c6ddf
Thapelo Bapela <75887730+thapelobaps@users.noreply.github.com>
2026-07-18 09:29:24 +0200

    Use SQL account identifiers in account cards

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -1,14 +1,13 @@
 import { formatAmount } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
 import Copy from './Copy'
 import { CreditCardProps } from '@/types'
 
 const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) => {
   return (
     <div className="flex flex-col">
-      <Link href={`/transaction-history/?id=${account.appwriteItemId}`} className="bank-card">
+      <Link href={`/transaction-history/?id=${account.id}`} className="bank-card">
         <div className="bank-card_content">
           <div>
             <h1 className="text-16 font-semibold text-white">
@@ -29,7 +28,7 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
               </h2>
             </div>
             <p className="text-14 font-semibold tracking-[1.1px] text-white">
-              ●●●● ●●●● ●●●● <span className="text-16">{account?.mask}</span>
+              ●●●● ●●●● ●●●● <span className="text-16">{account.mask}</span>
             </p>
           </article>
         </div>
@@ -59,8 +58,8 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
         />
       </Link>
 
-      {showBalance && account.shareableId && (
-        <Copy title={account.shareableId} label="Demo account reference" />
+      {showBalance && account.demoReference && (
+        <Copy title={account.demoReference} label="Demo account reference" />
       )}
     </div>
   )
