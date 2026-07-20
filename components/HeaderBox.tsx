6eee8b0f39208b2c61ce5c0737bf6174c70e93bd
Thapelo Bapela <75887730+thapelobaps@users.noreply.github.com>
2026-07-20 15:56:28 +0200

    Keep dashboard greeting styling self-contained

--- components/HeaderBox.tsx
+++ components/HeaderBox.tsx
@@ -5,7 +5,7 @@ const HeaderBox = ({ type = 'title', title, subtext, user }: HeaderBoxProps) => 
     <div className="header-box">
       <h1 className="header-box-title">
         <span>{title}</span>
-        {type === 'greeting' && user ? <span className="kape-greeting-name"> {user}</span> : null}
+        {type === 'greeting' && user ? <span className="text-[#7a4a37]"> {user}</span> : null}
       </h1>
       <p className="header-box-subtext">{subtext}</p>
     </div>
