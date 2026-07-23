7b735f1ea08886473bd457073f3c0cef9cf04120
Thapelo Bapela <75887730+thapelobaps@users.noreply.github.com>
2026-07-23 12:47:03 +0200

    Register demo pay-in provider

--- backend/Kape.Api/Configuration/ServiceCollectionExtensions.cs
+++ backend/Kape.Api/Configuration/ServiceCollectionExtensions.cs
@@ -184,6 +184,7 @@ public static class ServiceCollectionExtensions
             };
         });
         services.AddSingleton<IPaymentTokenizationProvider, DemoPaymentTokenizationProvider>();
+        services.AddSingleton<IPayInProvider, DemoPayInProvider>();
         services.AddSingleton<IDigitalProductProvider, DemoDigitalProductProvider>();
 
         services.AddHostedService<WalletQueueWorker>();
